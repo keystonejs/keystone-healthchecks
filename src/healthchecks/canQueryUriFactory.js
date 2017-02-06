13fbe52e17bb776c9b30b812dced6de5b6d3f519
Ben Conolly <ben@thinkmill.com.au>
2017-02-06 14:01:32 +1100

    fixup

--- src/healthchecks/canQueryUriFactory.js
+++ src/healthchecks/canQueryUriFactory.js
@@ -1,7 +1,11 @@
 const canQueryUri = require('./canQueryUri');
 
-module.exports = function canQueryUriFactory (uri, siteName) {
+module.exports = function canQueryUriFactory (uri, siteName, timeout) {
 	return class canQueryUriImpl extends canQueryUri {
+		constructor (timeout) {
+			super(timeout);
+		}
+		
 		get uri () { return uri; }
 		get siteName () { return siteName || uri; }
 	};
