6e2e5caf9bfe34cc1ffb11566497f7465bcacb3f
Ben Conolly <ben@thinkmill.com.au>
2017-02-06 13:59:56 +1100

    fixup

--- src/healthchecks/canQueryUri.js
+++ src/healthchecks/canQueryUri.js
@@ -5,12 +5,12 @@ const url = require('url');
 
 
 module.exports = class canQueryUri extends Healthcheck {
+	constructor (timeout) {
+		this.timeout = timeout || 3000;
+	}
+	
 	get name () {
 		return `Can Query site: ${this.siteName}`;
-	}
-
-	get timeout () {
-		return 3000;
 	}
 
 	resolver () {
@@ -20,25 +20,21 @@ module.exports = class canQueryUri extends Healthcheck {
 			client.get(this.uri, res => {
 				if (res.statusCode === 200) return resolve(`${this.siteName} is available`);
 				
-				var body = '';
-		        res.on('data', (chunk) => body += chunk);
 				res.on('end', () => {
 					return reject({
 						status: `${res.statusCode} ${res.statusMessage}`,
-						body: body,
+						uri: this.uri,
+					});
+				})
+				// http and https treat an inability to hit the server or timeouts
+				// differently so they throw an error instead of being passed to
+				// the callback. This hook handles this.
+				.on('error', (err) => {
+					return reject({
+						error: err,
 						uri: this.uri,
 					});
 				});
-			})
-			// http and https treat an inability to hit the server or timeouts
-			// differently so they throw an error instead of being passed to
-			// the callback. This hook handles this.
-			.on('error', (err) => {
-				return reject({
-					error: err,
-					uri: this.uri,
-				});
-			});
 		});
 	}
 };
